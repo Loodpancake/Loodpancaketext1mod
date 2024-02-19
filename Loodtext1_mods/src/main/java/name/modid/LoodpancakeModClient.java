b6a9233f2a95150021c509ff3207f8fe540af789
Loodpancake <2103180741@qq.com>
2024-02-19 17:56:45 +0800

    Inital commit

---
+++ Loodtext1_mods/src/main/java/name/modid/LoodpancakeModClient.java
@@ -0,0 +1,10 @@
+package name.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LoodpancakeModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
